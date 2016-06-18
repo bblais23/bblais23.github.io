c6156b3ddb7cf6d00d9411722c382519ae792646
bblais23 <ben.blais6@gmail.com>
2016-06-18 11:38:23 -0400

    Added paramiters to template library, abstracted calling

--- node/htmlTemplatize.js
+++ node/htmlTemplatize.js
@@ -4,36 +4,77 @@
   * you may not use this file except in compliance with the License. 
   */
 var fs = require('fs');
+var path = require('path');
 
 module.exports = {};
 
-var commandRegex = /\{\{([a-zA-Z0-9\.\s]*)\}\}/g;
+var commandRegex = /\{\{([a-zA-Z0-9\.\s]*)(\([a-zA-Z0-9\.\s,]*\))?\}\}/g;
 
-var internalCommands = {}
+var internalConfig = {};
+
+var internalCommands = {};
+
+internalConfig.encoding = 'UTF-8';
 
 internalCommands.body = function(context)
 {
   return context.partial;
 }
 
-//Both are strings, can do file reading later
-module.exports.templatize = function(template, partial, context) 
+internalCommands.include = function(context, args)
 {
-  //Define the context (passed into internal commands)
-  context = context || {};
+  var templatePath = '',
+      includePath,
+      includeContent;
+  //There should be only one argument, if less just return
+  if(args.length < 1)
+  {
+    return null;
+  }
 
-  context.template = template;
-  context.partial = partial;
+  includePath = args[0].trim();
+  //Try to find the directory
+  if(context.templatePath)
+  {
+    //Get dir pathname
+    templatePath = path.dirname(context.templatePath);
+  }
 
+  //If this is not a full path then append the path to the templatePath
+  if(!/^([A-Za-z]:)?[\\\/]/.test(includePath))
+  {
+    includePath = templatePath + "/" + includePath;
+  }
+
+  //Read in the file (assume utf-8 encoding because FUCK EVERYTHING!)
+  includeContent = fs.readFileSync(includePath, {encoding : internalConfig.encoding});
+
+  return includeContent;
+};
+
+var copyContext = function(context)
+{
+  var ret = {};
+  var prop;
+  for(prop in context)
+  {
+    ret[prop] = context[prop];
+  }
+
+  return ret;
+}
+
+//Both are strings, can do file reading later
+var templatizeInternal = function(context) 
+{
   //Simple function handling the find and replace
-  //TODO add arguments
   var result = template.replace(commandRegex, function(match, command, argumentList)
   {
     var args = [];
     //get the argument list
     if(argumentList.length > 2)
     {
-      args = argumentList.substring(1,argumentList.length).split(,);
+      args = argumentList.substring(1,argumentList.length).split(',');
     }
 
     if(internalCommands[command])
@@ -44,4 +85,49 @@ module.exports.templatize = function(template, partial, context)
   });
 
   return result;
+};
+
+module.exports.templatize = function(arg1, arg2, outputPrefix){
+  //First check to see if we are dealing with text or a path
+  var context = {};
+  var partials;
+
+  //Only run this function if both arguments are present.
+  if(!arg1 || !arg2 || !outputPrefix)
+  {
+    throw "You must include all three arguments in this function call.";
+  }
+
+  context.template = fs.readFileSync(arg1, {encoding : internalConfig.encoding});
+  context.templatePath = arg1;
+
+  //Now handle the partial(s)
+  if(arg2.constructor === Array)
+  {
+    partials = arg2;
+  }
+  else
+  {
+    partials = [arg2];
+  }
+
+  //Loop through the paritals
+  paritals.foreach(function(partial){
+    var ctx = copyContext(context);
+    ctx.partialPath = partial;
+
+    ((ctx_internal) => {
+      fs.readFile(ctx_internal.partialPath, {encoding: internalConfig.encoding}, (err, data) =>{
+        if(error)
+        {
+          console.error("Could not read parial file: " + ctx_internal.partialPath);
+        }
+        else
+        {
+          ctx_internal.parial = data;
+          templatizeInternal(ctx_internal);
+        }
+      });
+    })(ctx);
+  });
 };
