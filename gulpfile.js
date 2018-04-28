73589028dcc8536783cac53af305a6e291434c80
bblais23 <ben.blais6@gmail.com>
2018-04-27 20:26:08 -0400

    added templateizer config to web build

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 const gulp = require("gulp");
+const templatize = require("../Templateizer/templateizer-gulp.js");
 
-gulp.task('default', _ => {
-    
+gulp.task('pages', _ => {
+    return gulp.src('src/partials/**/*.html')
+               .pipe(templatize('src/template.html'))
+               .pipe(gulp.dest('./build'));
 });
+
+gulp.task('default', ['pages']);
