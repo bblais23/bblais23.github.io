4fd5e6eec4411f8703d169921604ea24ae61725e
bblais23 <ben.blais6@gmail.com>
2018-04-28 11:17:26 -0400

    update copyright and add working drawer

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,8 @@
+/**
+  * Copyright 2018 Benjamin Blais
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License. 
+  */
 const gulp = require("gulp");
 const templatize = require("../Templateizer/templateizer-gulp.js");
 
