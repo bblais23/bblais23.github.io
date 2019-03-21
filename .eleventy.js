f032864c2e9db41be4d0bc398299db596e1c8a2d
bblais23 <ben.blais6@gmail.com>
2019-03-20 20:20:19 -0400

    Add eleventy and config

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = {
+    dir: {
+      input: 'src',
+      output: '_site'
+    },
+    templateFormats: [
+      "md",
+      "html",
+      "css",
+      "jpg",
+      "png",
+      "gif",
+      "njk"
+    ],
+    passthroughFileCopy: true
+}
